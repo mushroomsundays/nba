004265b5b5b5db0636b0d47f0ae099f1e59f8406
mushroomsundays <mushroomsundays@protonmail.com>
2022-12-14 16:07:44 -0500

    new: file for source code loading s3 to rds database

---
+++ etl/s3_to_db.py
@@ -0,0 +1,16 @@
+import json, time, logging
+import numpy as np
+import pandas as pd
+
+"""
+Pulls JSON Player and team data from S3 and loads it into RDS database
+"""
+
+def player_career():
+    """
+    career_dict = career.get_dict()['resultSets'][0]
+    headers = career_dict['headers']
+    data = career_dict['rowSet']
+    career_df = pd.DataFrame(data, columns=headers)
+    """
+    pass
