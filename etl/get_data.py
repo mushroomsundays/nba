79853fd1c992d13639e4c04beb859ff407070772
mushroomsundays <mushroomsundays@protonmail.com>
2022-12-13 19:41:07 -0500

    changed file name

--- etl/get_data.py
+++ etl/get_data.py
@@ -8,16 +8,15 @@ from nba_api.stats.static import players, teams
 
 # id, full_name, first_name, last_name, is_active
 all_players = players.get_players()
-print(type(all_players))
-for i in range(30):
-    print(all_players[i])
-
 # id, full_name, abbreviation, nickname, city, state, year_founded
 all_teams = teams.get_teams()
-#for i in range(32):
-#    print(all_teams[i])
+
 
 json_players = json.dumps(all_players, indent=2)
+json_teams = json.dumps(all_teams, indent=2)
 with open("all_players.json", "w") as f:
     f.write(json_players)
+with open("all_teams.json", "w") as f:
+    f.write(json_teams)
+
 
